f6b1311e93ea72c6b8e886b4969e988e26dea0d7
davinavarro <davinavarro@2ndmarket.com.br>
2023-06-01 10:59:52 -0300

    routes were transfered to another directory

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
-
+const dotenv = require('dotenv').config();
 const app = express();
+const router = require('./routes/contactRoutes');
+const port = process.env.PORT || 5000;
 
-const port = 5000;
-
-app.get('/', (req, res)=>{
-    console.log(`GET request was made at route /`);
-    res.status(200).send('Hello, world!');
-});
+app.use('/api/contacts', router.router);
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
