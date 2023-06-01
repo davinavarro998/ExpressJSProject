56597e119ae9fce2ba78eebb005cd9f0f37f308b
davinavarro <davinavarro@2ndmarket.com.br>
2023-06-01 11:11:39 -0300

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const router = require('./routes/contactRoutes');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use('/api/contacts', router.router);
 
 app.listen(port, ()=>{
