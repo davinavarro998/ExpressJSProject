1e2032b472bdeb2cf3e693b6729980c6ee45d028
davinavarro <davinavarro@2ndmarket.com.br>
2023-06-02 15:29:48 -0300

    To do list

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const app = express();
 const path = require('path');
 
 app.engine('html', require('ejs').renderFile);
-/* app.set('view engine', 'html');
+app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views')); */
+app.set('views', path.join(__dirname, '/views')); 
+
+
+let peopleGroup = ['Jason', 'Kimberly', 'Trini', 'Billy', 'Zack', 'Tommy'];
+
 
 app.get('/', (req, res)=>{
     console.log('GET request');
-    res.render('index.html', {jason:`Lee Scott`});
+    res.render('index.html', {peopleGroup:peopleGroup});
+});
+
+app.get('/delete/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(`GET request at /delete/${id}`);
+    peopleGroup = peopleGroup.filter((value, position)=>{
+        return position !== id;
+    });
+    res.render('index.html', {peopleGroup:peopleGroup});
 });
 
 app.listen(port, ()=>{
