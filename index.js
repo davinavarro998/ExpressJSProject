8a4c7902b5aac1659e54e40a30149e4f60436e91
davinavarro <davinavarro@2ndmarket.com.br>
2023-06-02 10:25:20 -0300

    new project

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
-const cors = require('cors');
+const port = 3000;
 const app = express();
-const router = require('./routes/contactRoutes');
-const port = process.env.PORT || 5000;
+const path = require('path');
 
-app.use(cors());
-app.use('/api/contacts', router.router);
+app.engine('html', require('ejs').renderFile);
+/* app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views')); */
+
+app.get('/', (req, res)=>{
+    console.log('GET request');
+    res.render('index.html', {jason:`Lee Scott`});
+});
 
 app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
