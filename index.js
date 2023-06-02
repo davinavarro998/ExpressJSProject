65b5b550a0b3eafb4a0b73d39d3501ddae8340ec
davinavarro <davinavarro@2ndmarket.com.br>
2023-06-02 15:56:10 -0300

    to do list complete

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
+//app.use( bodyParser().json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -11,6 +14,14 @@ app.set('views', path.join(__dirname, '/views'));
 
 let peopleGroup = ['Jason', 'Kimberly', 'Trini', 'Billy', 'Zack', 'Tommy'];
 
+
+
+
+app.post('/', (req, res)=>{
+    const task = req.body;
+    peopleGroup.push(task.myTask);
+    res.render('index.html', {peopleGroup:peopleGroup});
+});
 
 app.get('/', (req, res)=>{
     console.log('GET request');
